826b22f9437b88a742a60e0d7a093a498ef971b3
Siddharth Gangwar <sidgangs99@gmail.com>
2022-03-26 11:54:51 +0530

    feat: Remove footer.

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,13 +7,13 @@ import Layout from "../components/layout";
 const IndexPage = () => {
   return (
     <div className="font-mono">
-      <div className="sticky top-0 h-10 w-full flex items-center justify-around bg-zinc-800 text-zinc-400 z-50">
+      <div className="sticky top-0 z-50 flex h-10 w-full items-center justify-around bg-zinc-800 text-zinc-400">
         <Header />
       </div>
-      <div className="snap-mandatory snap-y overflow-scroll h-screen bg-custom-color-300">
+      <div className="bg-custom-color-300 h-screen snap-y snap-mandatory overflow-scroll">
         <Layout />
       </div>
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 };
